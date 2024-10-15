78e50b9c6d3248b1c91128974876a5466ed844cc
mijail-ayala <155578782+mijail-ayala@users.noreply.github.com>
2024-10-15 17:51:10 +0200

    Update Main.java

--- Ex0/src/Main.java
+++ Ex0/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Objecte persona1 = Objecte.getInstancia();
+        objecte persona1 = objecte.getInstancia();
         persona1.configurar("Laura", "López", 24);
         System.out.println(persona1);
 
         Thread.sleep(3000);
 
-        Objecte persona2 = Objecte.getInstancia();
+        objecte persona2 = objecte.getInstancia();
         persona2.configurar("Sergio", "Ramírez", 30);
         System.out.println(persona2);
 
         Thread.sleep(3000);
 
-        Objecte persona3 = Objecte.getInstancia();
+        objecte persona3 = objecte.getInstancia();
         persona3.configurar("Claudia", "Hernández", 22);
         System.out.println(persona3);
     }
